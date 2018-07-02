5f66fc0eafa7a4fed7f672c55a92c83e427bce47
rickywjr <40745547+rickywjr@users.noreply.github.com>
2018-07-02 10:48:01 +1000

    RW-Test
    Testing

---
+++ Spellthief/Assets/Scripts/BulletForce.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletForce : MonoBehaviour {
+
+    public float Magnitude;
+    public float Lifetime;
+    public int Damage = 1; // bullet damage on collision
+
+    // Use this for initialization
+    void Start () {
+        Rigidbody RB = GetComponent<Rigidbody>();  //specify bullets rigid body
+        RB.AddForce(transform.forward * Magnitude); // add force to rigid body in forwards directio
+        Destroy(gameObject ,Lifetime); // after the lifetime expires destroy bullety
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject); //destruct on colliding
+    }
+}
---
+++ Spellthief/Assets/Scripts/Movement2D.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2D : MonoBehaviour {
+    public float speed = 2;
+
+	void Update () {
+
+        // uses  WASD + scroll wheel for 3D Movement
+        float vert = Input.GetAxis("Horizontal") * Time.deltaTime * speed; // X axis movement
+        float horiz = Input.GetAxis("Vertical") * Time.deltaTime * speed; // Z axis movement
+        //float deep = Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * speed; // y Axis Movement  ( diabled for 2D)
+        transform.Translate(vert, 0, horiz); 
+    }
+}
+        
