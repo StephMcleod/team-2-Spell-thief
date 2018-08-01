aa38d2a49a9a322923e5be825b9ef24638ab5415
NielStevensen <1011927@student.sae.edu.au>
2018-08-01 12:53:36 +1000

    Added Basic ice spell

--- Spell Thief 2.0/Assets/Scripts/Player + Spells/SpellHit.cs
+++ Spell Thief 2.0/Assets/Scripts/Player + Spells/SpellHit.cs
@@ -7,17 +7,27 @@ public class SpellHit : MonoBehaviour {
     public float Magnitude;
     public float Lifetime;
     public int Damage = 1; // bullet damage on collision
+    public float Bounces;
+    public Rigidbody2D RB;
 
     // Use this for initialization
     void Start () {
-        Rigidbody2D RB = GetComponent<Rigidbody2D>();  //specify bullets rigid body
+        RB = GetComponent<Rigidbody2D>();  //specify bullets rigid body
         RB.AddForce(transform.right * Magnitude); // add force to rigid body in forwards direction
         Destroy(gameObject ,Lifetime); // after the lifetime expires destroy bullety
 	}
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(gameObject); //destruct on colliding
+        int xMultiply = Random.Range(-1, 2);
+        int yMultiply = Random.Range(-1, 2);
+        Bounces -= 1;
+        RB.velocity = new Vector2(RB.velocity.x * xMultiply, RB.velocity.y * yMultiply);
+        if (Bounces <= 0)
+        {
+            Destroy(gameObject); //destruct on colliding
+        }
+        // collision.point relatice to transform
     }
 }
     
