513641b67cf71720d90e8bb40e29e5ca2cf4e2a8
Nielstevensen <Stevennielsen4@gmail.com>
2018-08-05 18:05:55 +1000

    Added Lighning Beam, Started Ice Beam

---
+++ Spell Thief 2.0/Assets/Scripts/Spell Effects/Freeze.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Freeze : MonoBehaviour {
+
+    public float Life;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (GetComponentInChildren<EnemySpellCast>() != null ) GetComponentInChildren<EnemySpellCast>().enabled = false; // disable attacking
+        if (GetComponentInChildren<MeeleAttack>() != null) GetComponent<MeeleAttack>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if ( Life <= 0) // when the enemy thaws out
+        {
+            Destroy(this);
+        }
+	}
+
+    void OnDestroy()
+    {
+        if (GetComponentInChildren<EnemySpellCast>() != null) GetComponentInChildren<EnemySpellCast>().enabled = true; // enable attacking
+        if (GetComponentInChildren<MeeleAttack>() != null) GetComponent<MeeleAttack>().enabled = true;
+    }
+}
---
+++ Spell Thief 2.0/Assets/Scripts/Spell Effects/LightningBeam.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningBeam : MonoBehaviour {
+
+    public Transform Player;
+    public LineRenderer lineDraw;
+    private RaycastHit2D Hit;
+    public LayerMask Layers;
+    public float Damage;
+    public float cost;
+    public float ArcRange;
+
+    // Use this for initialization
+    void Start () {
+        lineDraw = GetComponent<LineRenderer>(); // line render component
+        Player = GameObject.Find("PlayerCaster").transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        Hit = Physics2D.Raycast(Player.transform.position, Player.transform.right,100,Layers);
+        if (Hit.transform != null)
+        {
+            if (Hit.transform.GetComponent<HealthController>() != null) // if there is a target
+            {
+                Hit.transform.gameObject.GetComponent<HealthController>().Health -= Damage; // sybtract 1 health each frame
+                Hit.transform.gameObject.GetComponent<HealthController>().Invoke("DrawHealth", 0); // redraw health
+            }
+            if (GetComponent<CreateFlame>() != null)
+            {
+                GetComponent<CreateFlame>().Hitpoint = Hit.point;
+                GetComponent<CreateFlame>().Invoke("CreateFire", 0);
+            }
+        }
+        lineDraw.SetPosition(0, Player.position); // set line start at player
+        lineDraw.SetPosition(1, Player.right * 100); //draw line 100 units
+        
+        if (Hit.transform != null)
+        {
+            lineDraw.SetPosition(1, Hit.point); // stop line if something is hit
+            RaycastHit2D ArcTarget = Physics2D.CircleCast(Hit.point, ArcRange, transform.forward, ArcRange, Layers);
+            if (Physics2D.CircleCast(Hit.point,ArcRange,transform.forward, ArcRange,Layers))
+            {
+                lineDraw.SetPosition(2, ArcTarget.transform.position);
+            }
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            Destroy(gameObject);
+        }
+        else if (GameObject.Find("Player").GetComponent<ManaBar>().Mana >= cost)
+        {
+            GameObject.Find("Player").GetComponent<ManaBar>().Mana -= cost;
+        }
+    }
+}
