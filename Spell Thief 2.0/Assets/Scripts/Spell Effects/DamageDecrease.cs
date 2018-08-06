083382fdbe310ca9db0ce76303b395d38416abca
NielStevensen <1011927@student.sae.edu.au>
2018-08-06 12:41:46 +1000

    Added LightningSpell
    Added ligtning basic projectile

---
+++ Spell Thief 2.0/Assets/Scripts/Spell Effects/DamageDecrease.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDecrease : MonoBehaviour {
+
+    public int DeteroationRate;
+    public float WaitTime;
+
+    // Update is called once per frame
+    void Update () {
+        WaitTime += Time.deltaTime;
+        if (GetComponent<SpellHit>().Damage >= 10)
+        {
+            if (WaitTime >= 2)
+            {
+                GetComponent<SpellHit>().Damage -= DeteroationRate;
+                WaitTime = 0;
+            }
+        }
+	}
+}
