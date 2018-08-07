03c2c219a6d4a5812c5a1738921721cd985bbea4
Nielstevensen <Stevennielsen4@gmail.com>
2018-08-07 15:32:59 +1000

    Finished ice beam + projectile Started teleport

--- Spell Thief 2.0/Assets/Scripts/Spell Effects/Freeze.cs
+++ Spell Thief 2.0/Assets/Scripts/Spell Effects/Freeze.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Freeze : MonoBehaviour {
 
-    public float Life;
+    public float Life = 5;
+    public float InitLife = 5;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         if (GetComponentInChildren<EnemySpellCast>() != null ) GetComponentInChildren<EnemySpellCast>().enabled = false; // disable attacking
         if (GetComponentInChildren<MeeleAttack>() != null) GetComponent<MeeleAttack>().enabled = false;
---
+++ Spell Thief 2.0/Assets/Scripts/Spell Effects/Retarget.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Retarget : MonoBehaviour {
+
+    public Rigidbody2D RB;
+    public GameObject Target;
+    public float magnitude;
+
+    // Use this for initialization
+    void Start () {
+        GameObject[] TargetList = GameObject.FindGameObjectsWithTag("Enemy");
+        Target = TargetList[0];
+        RB = GetComponent<Rigidbody2D>();
+        foreach(GameObject Targets in TargetList)
+        {
+            if((Targets.transform.position - transform.position).magnitude < (Target.transform.position - transform.position).magnitude)
+            {
+                Target = Targets;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Target != null)
+        {
+            RB.velocity = new Vector2(0, 0);
+            RB.AddForce((Target.transform.position - transform.position).normalized * magnitude);
+        }
+	}
+}
