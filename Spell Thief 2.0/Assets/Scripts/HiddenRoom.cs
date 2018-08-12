70bebcfab93fc5e72b8d10951e7122768b3beb4c
WilliamRobertKidd <40745570+WilliamRobertKidd@users.noreply.github.com>
2018-08-13 08:38:30 +1000

    Retouching greyboxes + timed platform script complete

---
+++ Spell Thief 2.0/Assets/Scripts/HiddenRoom.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenRoom : MonoBehaviour {
+
+    public float alpha = 0;
+    public Color spriteColor;
+
+    // Use this for initialization
+    void Start ()
+    {
+        spriteColor = GetComponent<SpriteRenderer>().color;
+        alpha = spriteColor.a;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        alpha = 0;
+        spriteColor.a = alpha;
+
+        GetComponent<SpriteRenderer>().color = spriteColor;
+    }
+}
