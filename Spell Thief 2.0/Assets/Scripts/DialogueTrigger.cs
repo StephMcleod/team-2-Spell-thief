b6fce7219d5d42661b310086cf1cc8f60b343f0d
WilliamRobertKidd <40745570+WilliamRobertKidd@users.noreply.github.com>
2018-08-14 21:16:06 +1000

    Extra greybox edits

---
+++ Spell Thief 2.0/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueTrigger : MonoBehaviour {
+
+    public float timeLeft;
+    public float resetTime;
+    public GameObject dialogueBox;
+    public string dialogue;
+    public Text dialogueText;
+    private bool hit;
+
+    // Use this for initialization
+    void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (hit == true)
+        {
+            timeLeft -= Time.deltaTime;
+            dialogueBox.SetActive(true);
+            dialogueText.text = dialogue;
+            if (timeLeft < 0)
+            {
+                dialogueBox.SetActive(false);
+                timeLeft = resetTime;
+                Destroy(gameObject);
+            }
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        hit = true;
+    }
+}
---
+++ Spell Thief 2.0/Assets/Scripts/PitfallSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitfallSpawner : MonoBehaviour {
+
+    public float timeLeft;
+    public float resetTime;
+    public GameObject pitfallEnemy;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is ca
+	void Update ()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            Instantiate(pitfallEnemy, gameObject.transform);
+            timeLeft = resetTime;
+        }
+    }
+
+
+}
