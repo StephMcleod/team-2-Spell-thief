422cf557bb41d955c887e8351c147a0a9c130e1e
WilliamRobertKidd <40745570+WilliamRobertKidd@users.noreply.github.com>
2018-08-15 21:06:32 +1000

    Greybox edits + new scripts, see description
    Start/Finish Triggers added + Explosive Block Script + Level 3 Moving Platforms + Sound Playing During Hidden Room Trigger

---
+++ Spell Thief 2.0/Assets/Scripts/ExplosionBlock.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionBlock : MonoBehaviour {
+
+    public float timeLeft;
+    public float resetTime;
+
+    public int breakStepsTotal;
+    public int currentStep;
+    public GameObject sourceBlock;
+    public GameObject[] breakBlocks;
+
+    public int currentBreakBlock;
+
+    public int[] breakBlocksThisStep;
+    public int i;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (GetComponent<BoxCollider2D>().enabled == false)
+        {
+            if (currentStep < breakStepsTotal)
+            {
+                timeLeft -= Time.deltaTime;
+                if (timeLeft < 0)
+                {
+                    breakingPhase();
+                    timeLeft = resetTime;
+                }
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    void breakingPhase()
+    {
+            for (i = 0; i < breakBlocksThisStep[currentStep]; i++)
+            {
+                Destroy(breakBlocks[currentBreakBlock]);
+                ++currentBreakBlock;
+            }
+            ++currentStep;
+    }
+
+
+}
--- Spell Thief 2.0/Assets/Scripts/LevelElements/HiddenRoom.cs
+++ Spell Thief 2.0/Assets/Scripts/LevelElements/HiddenRoom.cs
@@ -6,13 +6,15 @@ public class HiddenRoom : MonoBehaviour {
 
     public float alpha = 0;
     public Color spriteColor;
+    public AudioSource bricks;
 
     // Use this for initialization
     void Start ()
     {
         spriteColor = GetComponent<SpriteRenderer>().color;
         alpha = spriteColor.a;
-    }
+        bricks = GetComponent<AudioSource>();
+}
 	
 	// Update is called once per frame
 	void Update ()
@@ -25,6 +27,7 @@ public class HiddenRoom : MonoBehaviour {
         alpha = 0;
         spriteColor.a = alpha;
 
+        bricks.Play();
         GetComponent<SpriteRenderer>().color = spriteColor;
     }
 }
